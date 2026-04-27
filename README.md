f0d8744966add6f53072b1904cb045028d766195
Nicolai Søborg <git@xn--sb-lka.org>
2026-04-27 22:26:47 +0200

    Use PKCS#11 lib from commandline + go fmt

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # pkcs11-tester
+
+```bash
+go run main.go /usr/lib64/opensc-pkcs11.so
+
+go build
+./pkcs11-tester /usr/lib64/opensc-pkcs11.so
+```
+
+This will connect to a PKCS#11 token using the provided library, it will then open a session to _slot_ 0 (edit the code to change) and login using PIN from stdin.
+
+If login is successful, it will search for a key with these properties:
+
+|Property                | Value     |
+| ---------------------- | --------- |
+|`CKA_EC_PARAMS`         |`id-X25519`|
+|`CKA_CLASS`             |`CKO_PRIVATE_KEY`|
+|`CKA_ALLOWED_MECHANISMS`|`CKM_ECDH1_DERIVE`|
+|`CKA_DERIVE`            |true|
+|`CKA_TOKEN`             |true|
+
+If such an object is found, it will try to find the corresponding public key having these values:
+
+|Property                | Value     |
+| ---------------------- | --------- |
+|`CKA_EC_PARAMS`         |`id-X25519`|
+|`CKA_CLASS`             |`CKO_PUBLIC_KEY`|
+|`CKA_TOKEN`             |true|
+|`CKA_ID`                | {value from private key, probably blank} |
+
+If both private and public key is found, then public key will be printed (base64 encoded) and to check that `CKM_ECDH1_DERIVE` works, the tool will derive a shared secret using `ECDH(secret, public)`
--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/hex"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/miekg/pkcs11"
@@ -13,19 +13,26 @@ import (
 
 const (
 	SLOT = 0
-	//HsmPath = "/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so"
-	HsmPath = "/usr/lib64/opensc-pkcs11.so"
+	//hsmPath = "/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so"
+	//hsmPath = "/usr/lib64/opensc-pkcs11.so"
+	//hsmPath = "/opt/BouncyHsm/artifacts/.tmp/native/Linux-x64/BouncyHsm.Pkcs11Lib.so"
 
-	CURVE25519_OID_RAW = "06032B656E"  // 1.3.101.110 ("id-X25519")
 	NoiseKeySize = 32
 )
 
 func main() {
-	module, err := p11.OpenModule(HsmPath)
-	if err != nil {
-		panic(fmt.Errorf("failed to load module library: %s", HsmPath))
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <path to PKCS#11 library>\n", os.Args[0])
+		os.Exit(1)
 	}
-	fmt.Printf("Module '%s' loaded correctly\n", HsmPath)
+	hsmPath := os.Args[1]
+
+	module, err := p11.OpenModule(hsmPath)
+	if err != nil {
+		panic(fmt.Errorf("[ERR] failed to load module library: %s", hsmPath))
+	}
+	fmt.Printf("[OK] Module '%s' loaded correctly\n", hsmPath)
+	defer module.Destroy()
 
 	slots, err := module.Slots()
 	if err != nil {
@@ -34,15 +41,16 @@ func main() {
 	fmt.Printf("Found %d slots\n", len(slots))
 
 	if uint(len(slots)) <= SLOT {
-		panic(fmt.Errorf("Requested slot (%d) but only %d available", SLOT, len(slots)))
+		panic(fmt.Errorf("[ERR] Requested slot (%d) but only %d available", SLOT, len(slots)))
 	}
 
 	// try to open a session on the slot
 	session, err := slots[SLOT].OpenSession()
 	if err != nil {
-		panic(fmt.Errorf("failed to open session on slot %d", SLOT))
+		panic(fmt.Errorf("[ERR] failed to open session on slot %d", SLOT))
 	}
-	fmt.Printf("OpenSession on slot %d worked\n", SLOT)
+	fmt.Printf("[OK] OpenSession on slot %d worked\n", SLOT)
+	defer session.Close()
 
 	// try to login to the slot
 	fmt.Printf("Enter Pin for slot %d:\n", SLOT)
@@ -50,53 +58,55 @@ func main() {
 	pin := strings.TrimSpace(string(userPin))
 	err = session.Login(pin)
 	if err != nil {
-		panic(fmt.Errorf("Login unsuccessful: %w", err))
+		panic(fmt.Errorf("[ERR] Login unsuccessful: %w", err))
 	}
-	fmt.Printf("Login successful\n")
+	fmt.Printf("[OK] Login successful\n")
+	defer session.Logout()
 
-	rawOID, _ := hex.DecodeString(CURVE25519_OID_RAW)
+	var X25519_OID = []byte{0x06, 0x03, 0x2b, 0x65, 0x6e} // "06032B656E"  // 1.3.101.110 ("id-X25519")
 
 	privateAttrs := []*pkcs11.Attribute{
-		pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, rawOID),
+		pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, X25519_OID),
 		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PRIVATE_KEY),
-		//pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC),
-		pkcs11.NewAttribute(pkcs11.CKA_DERIVE, true),      // private key should be allowed to derive a shared secret
-		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),       // look only for "token objects" (persisted on HSM)
+		//pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC_MONTGOMERY),
+		pkcs11.NewAttribute(pkcs11.CKA_ALLOWED_MECHANISMS, pkcs11.CKM_ECDH1_DERIVE),
+		pkcs11.NewAttribute(pkcs11.CKA_DERIVE, true), // private key should be allowed to derive a shared secret
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),  // look only for "token objects" (persisted on HSM)
 	}
 
 	// FindObject expects a single key with above attrs, otherwise it returns err
 	privateKey, err := session.FindObject(privateAttrs)
 	if err != nil {
-		panic(fmt.Errorf("Could not find private key with attrs: %w", err))
+		panic(fmt.Errorf("[ERR] Could not find private key with attrs: %w", err))
 	}
-	fmt.Printf("Found privateKey ref: %x\n", privateKey)
+	fmt.Printf("[OK] Found privateKey ref: %x\n", privateKey)
 
-	ckaId, err := privateKey.Attribute(pkcs11.CKA_ID);
+	ckaId, err := privateKey.Attribute(pkcs11.CKA_ID)
 	if err != nil {
-		panic(fmt.Errorf("Could not find CKA_ID of private key: %w", err))
+		panic(fmt.Errorf("[ERR] Could not find CKA_ID of private key: %w", err))
 	}
-	fmt.Printf("Found CKA_ID of privateKey: %s\n", ckaId)
+	fmt.Printf("[OK] Found CKA_ID of privateKey: %s\n", ckaId)
 
 	publicAttrs := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, X25519_OID), // public key be specified on the id-X25519 curve
 		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
 		//pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC),
-		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true),       // look only for "token objects" (persisted on HSM)
-		pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, rawOID), // public key be specified on the id-X25519 curve
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, true), // look only for "token objects" (persisted on HSM)
 		pkcs11.NewAttribute(pkcs11.CKA_ID, ckaId),
 	}
 
 	publicKey, err := session.FindObject(publicAttrs)
 	if err != nil {
-		panic(fmt.Errorf("Could not find public key: %w", err))
+		panic(fmt.Errorf("[ERR] Could not find public key: %w", err))
 	}
 
-	fmt.Printf("Found publicKey ref: %x\n", publicKey)
+	fmt.Printf("[OK] Found publicKey ref: %x\n", publicKey)
 
 	// From my understanding, for X25519 the public key is not stored
 	// in `CKA_VALUE` but instead in attribute `CKA_EC_POINT`.
 	// "DER-encoding of the public key value in little endian order as defined in RFC 7748"
 	// - https://docs.oasis-open.org/pkcs11/pkcs11-curr/v3.0/cs01/pkcs11-curr-v3.0-cs01.html
-	pubKeyVal, err := publicKey.Attribute(pkcs11.CKA_EC_POINT);
+	pubKeyVal, err := publicKey.Attribute(pkcs11.CKA_EC_POINT)
 	if err != nil {
 		panic(err)
 	}
@@ -104,53 +114,43 @@ func main() {
 		// On a Nitrokey Start, this gets the full EC_POINT value of 34 bytes instead of 32,
 		// If prefix is "04 (OCTET STRING) 20 (of length 0x20)" then discard the prefix
 		// TODO: Probably this is correct and the returned value should always be 34 bytes!?
-		if len(pubKeyVal) == NoiseKeySize + 2 && pubKeyVal[0] == 0x04 && pubKeyVal[1] == 0x20 {
+		if len(pubKeyVal) == NoiseKeySize+2 && pubKeyVal[0] == 0x04 && pubKeyVal[1] == 0x20 {
 			pubKeyVal = pubKeyVal[2:]
 		} else {
-			panic(fmt.Errorf("Key of wrong size returned (%d): %x", len(pubKeyVal), pubKeyVal))
+			panic(fmt.Errorf("[ERR] Key of wrong size returned (%d): %x", len(pubKeyVal), pubKeyVal))
 		}
 	}
 
-	fmt.Printf("Public Key Val (EC_POINT): %x\n", pubKeyVal)
-	fmt.Printf("Public Key Val (EC_POINT), b64: %s\n", base64.StdEncoding.EncodeToString(pubKeyVal))
+	fmt.Printf("[OK] Public Key (base64): %s\n", base64.StdEncoding.EncodeToString(pubKeyVal))
 
-    // derive a shared secret using (publicKey, privateKey) (so only shared between oneself)
+	// derive a shared secret using (publicKey, privateKey) (so only shared between oneself)
 	peerPubKey := pubKeyVal
-
-	var mech_mech uint = pkcs11.CKM_ECDH1_DERIVE
 
 	// before we call derive, we need to have an array of attributes which specify the type of
 	// key to be returned, in our case, it's the shared secret key, produced via deriving
 	// This template pulled from OpenSC pkcs11-tool.c line 4038
 	attrTemplate := []*pkcs11.Attribute{
-		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, false),
 		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_SECRET_KEY),
-		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_GENERIC_SECRET),
-		pkcs11.NewAttribute(pkcs11.CKA_SENSITIVE, false),
-		pkcs11.NewAttribute(pkcs11.CKA_EXTRACTABLE, true),
-		pkcs11.NewAttribute(pkcs11.CKA_ENCRYPT, true),
-		pkcs11.NewAttribute(pkcs11.CKA_DECRYPT, true),
-		pkcs11.NewAttribute(pkcs11.CKA_WRAP, true),
-		pkcs11.NewAttribute(pkcs11.CKA_UNWRAP, true),
+		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_GENERIC_SECRET), // output (shared secret) is a raw byte string with no structure ("generic secret")
+		pkcs11.NewAttribute(pkcs11.CKA_TOKEN, false),                        // we want to derive a "session object" (temporary for this session only)
+		pkcs11.NewAttribute(pkcs11.CKA_EXTRACTABLE, true),                   // shared secret can be exported to software
+		pkcs11.NewAttribute(pkcs11.CKA_SENSITIVE, false),                    // shared secret can be exported raw
 	}
 
 	// setup the parameters which include the peer's public key
 	ecdhParams := pkcs11.NewECDH1DeriveParams(pkcs11.CKD_NULL, nil, peerPubKey)
 
-	var mech *pkcs11.Mechanism = pkcs11.NewMechanism(mech_mech, ecdhParams)
+	var deriveMech *pkcs11.Mechanism = pkcs11.NewMechanism(pkcs11.CKM_ECDH1_DERIVE, ecdhParams)
 
 	// derive the secret key from the public key as input and the private key on the device
-	resultKey, err := p11.PrivateKey(privateKey).Derive(*mech, attrTemplate)
+	resultKey, err := p11.PrivateKey(privateKey).Derive(*deriveMech, attrTemplate)
 	if err != nil {
 		panic(err)
 	}
 	if len(resultKey) != NoiseKeySize {
-		panic(fmt.Errorf("Wrong size derived (%d)", len(resultKey)))
+		panic(fmt.Errorf("[ERR] Wrong size derived (%d)", len(resultKey)))
 	}
 
-	fmt.Printf("Derived key: %x\n", resultKey)
+	fmt.Printf("[OK] Derived key worked\n")
 
-	session.Logout()
-	session.Close()
-	module.Destroy()
 }
